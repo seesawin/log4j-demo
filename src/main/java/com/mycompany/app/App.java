ca10cf06e506b288f80b9b8e8bd7c2e8f0b3edf2
seesawin <seesawin@gmail.com>
2017-04-25 10:54:05 +0800

    new

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+import org.apache.log4j.Logger;
+
+public class App {
+	
+	private static final  Logger log= Logger.getLogger(App.class) ;
+	
+    public static void main( String[] args ) {
+    	log.info("Hello World!");
+    }
+}
---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+public class AppTest {
+	private static final  Logger log = Logger.getLogger(AppTest.class);
+	
+	@Test
+	public void test() {
+		log.info("Hello World!");
+	}
+}
